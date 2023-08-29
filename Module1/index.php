98dd81dbc6fe48db0c77a29b894e151d7c6bf8a5
UdayChinamora <uchinamhora@gmail.com>
2023-08-29 12:57:09 -0500

    initial commit

---
+++ Module1/index.php
@@ -0,0 +1,32 @@
+<?php 
+
+//echo'Hello,World <br>';
+//echo'Name : Uday Chinhamora <br>';
+//echo'Date: 28 August 2023 <br>';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My first php file</title>
+
+	<h1><?php 
+	echo'Hello,World <br>';
+	?>
+</h1>
+
+
+</head>
+<body>
+<?php 
+
+
+echo'Name : Uday Chinhamora <br>';
+echo'Date: 28 August 2023 <br>';
+
+?>
+</body>
+</html>
