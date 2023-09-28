a37d08b700a5151d519ab76261ec6d86bfdfd85e
Uday Chinhamora <uchinamhora@gmail.com>
2023-09-28 15:09:13 -0500

    added selectEvents file

---
+++ Assignments/selectEvents.php
@@ -0,0 +1,39 @@
+<?php
+require '..\Database\dbConnect.php';
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sql = "SELECT events_id,events_name,events_presenter FROM wdv341_events";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <script type="text/javascript"></script>
+  <style type="text/css">
+    
+    .myColor{
+      color:red;
+    }
+  </style>
+  <title></title>
+</head>
+<body>
+  <?php  
+
+while($row = $stmt->fetch() ) {
+echo"<p style='border:solid 1px black;width:50%;'>";
+ echo "Events ID  : ".$row["events_id"]."&ensp;&ensp;&ensp;&ensp;" ."Events  Name :  ".$row["events_name"]."&ensp;&ensp;&ensp;&ensp;". "Presenter : ".$row["events_presenter"]. "<br>";
+echo"</p>";
+  }
+?>
+</body>
+</html>
--- Assignments/wdv341.php
+++ Assignments/wdv341.php
@@ -20,8 +20,8 @@
             <ul>
                 <li><a href="../index.php">Home</a></li>
                 <li><a href="wdv341.php">Assignments</a></li>
-                <li><a href="../Database/dbConnect.php">Connect</a></li>
-                <li><a href="contantUs.php">Contact</a></li>
+                <li><a href="selectEvents.php">Connect</a></li>
+                <li><a href="../contactUs.php">Contact</a></li>
             </ul>
         </nav>
     </header>
